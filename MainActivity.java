38e709b45f7dc8f4e7532addd0dbc96eccb20a09
HelloWmz <1170155848@qq.com>
2017-05-29 11:11:46 +0800

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.wang.cn.jobdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
